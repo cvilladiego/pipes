f0d394c2fd8958b014ec4021ea6908739f4c74b8
Carlos Villadiego <carlovinar93@gmail.com>
2023-01-24 15:06:40 -0500

    se crearon los diferentes pipes en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre:string = 'Stewie Griffin';
+
+  nombre2:string = 'mEg gRiFfin';
+
+  arreglo = [1,2,3,4,5,6,7,8,9];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.235;
+
+  salario: number = 1800000.3;
+
+  fecha:Date = new Date();
+
+
+  personaje = {
+    nombre: 'Meg',
+    apellido: 'Griffin',
+    edad: 18,
+    dir:{calle: 'Spooner'},
+
+  }
+
+valorPromesa = new Promise<string>((resolve)=>{
+   setTimeout(() => {
+    resolve('data recibida');
+    
+   }, 4500);
+
+});
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule,
